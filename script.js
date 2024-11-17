3bb97c07817882131896325dc893c7f9efc651b7
Rassan99 <99618618+Rassan99@users.noreply.github.com>
2024-11-17 03:51:18 +0300

    Feat:User change the width

--- script.js
+++ script.js
@@ -1,30 +1,29 @@
 const mainDiv = document.querySelector("#mainDiv");
-console.log(mainDiv)
-function createGrid(num){
-    const gridWidth= (100/num)+'%';
+document.body.onload = () => createGrid(20);
+
+function createGrid(num) {
+    mainDiv.innerHTML = '';
+    const gridWidth = (100 / num) + '%';
     let total = num * num;
-    for ( let i = 1; i<=total ; i++){
+    for (let i = 1; i <= total; i++) {
         const newDiv = document.createElement("Div");
         mainDiv.appendChild(newDiv);
-        newDiv.className= "whiteGrid";
-        newDiv.style.flexBasis= gridWidth;
+        newDiv.className = "whiteGrid";
+        newDiv.style.flexBasis = gridWidth;
+    }
 
+    const gridItem = Array.from(document.getElementsByClassName("whiteGrid"));
+    for (let i = 0; i < gridItem.length; i++) {
+        gridItem[i].addEventListener('mouseover', () => {
+            gridItem[i].style.background = "pink";
+        });
     }
 }
-;
-document.body.onload = createGrid(20);
-const gridItem =Array.from(document.getElementsByClassName("whiteGrid"));
-// console.log(gridItem);
-// function changeColor (i){
-//     // console.log( gridItem[i]);
-//     // item.forEach(el => el.style.background = "red");
-//     gridItem[i].style.background = "green";
-// }
 
-// // gridItem.addEventListener("mouseover", changeColor(gridItem));
-
-for (let i = 0 ; i < gridItem.length; i++) {
-    gridItem[i].addEventListener('mouseover', () => {
-        gridItem[i].style.background = "pink";
-    })
- }
+function clickedButton() {
+    let userChoice = prompt("pick a width max of 100");
+    while (isNaN(userChoice) || userChoice <= 0 || userChoice > 100) {
+        userChoice = prompt("Invalid input! pick a width max of 100");
+    }
+    createGrid(userChoice);
+}
